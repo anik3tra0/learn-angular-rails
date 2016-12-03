aa178d444b20f8259dfed23a8898908bd870bd5f
anik3tra0 <anik3tra0@gmail.com>
2016-12-03 12:54:56 +0530

    Cap Production Configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,33 +4,25 @@ lock '3.6.1'
 set :application, 'visitor'
 set :repo_url, 'git@github.com:anik3tra0/learn-angular-rails.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :user,            'deploy'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+## Linked Files & Directories (Default None):
+set :linked_files, %w(config/database.yml config/secrets.yml )
+set :linked_dirs,  %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets public/uploads)
+set :bundle_binstubs, nil
 
-# Default value for :scm is :git
-# set :scm, :git
+namespace :puma do
+   desc 'Create Directories for Puma Pids and Socket'
+   task :make_dirs do
+      on roles(:app) do
+         execute "mkdir #{shared_path}/tmp/sockets -p"
+         execute "mkdir #{shared_path}/tmp/pids -p"
+      end
+   end
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+   before :start, :make_dirs
+end
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
